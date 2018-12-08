82f84979b51771606d6b6674041e3d4ec9b9807d
cat9563 <crysatl9563@gmail.com>
2018-12-08 11:43:23 -0500

    shows links to songs changed def for results to entity for clarity

--- follow_along.js
+++ follow_along.js
@@ -10,9 +10,19 @@ searchButton.addEventListener('click', function(event){//make ajax requet
         success: function (results){
             console.log(results)
             let resultsDiv = document.getElementById('search-results')
-            let countP = document.createElement('p')
+            resultsDiv.innerHTML = '' //clears stuff out 
+             let countP = document.createElement('p')
             countP.innerText = `Total count: ${results.total_count}`
             resultsDiv.appendChild(countP)
+
+            for (let repo of results.items){
+                let repoP = document.createElement("p") //creates a new element 
+                let repoLink =document.createElement('a') //found from console log 
+                repoLink.href = repo.html_url //had to look at log to get that also 
+                repoLink.innerText = repo.name //once again log 
+                repoP.appendChild(repoP) //puts the link inside repoP
+                resultsDiv.appendChild(repoP) //takes above line and appends it to the results div 
+            }
         }
     })
 })
--- itunes.js
+++ itunes.js
@@ -1,19 +1,33 @@
 const searchButton = document.getElementById('search-button')
 const searchField = document.getElementById('search-field')
 
-searchButton.addEventListener('click', function(event){//make ajax requet
+
+searchButton.addEventListener('click', function (event) {
     $.ajax({
         url: 'https://itunes-api-proxy.glitch.me/search?term=',
         data: {
             term: searchField.value
         },
         dataType: "json",
-        success: function (results){
-            console.log(results)
+        success: function (entity) {
+            console.log(entity)
             let resultsDiv = document.getElementById('search-results')
             let countP = document.createElement('p')
-            countP.innerText = `Total count: ${results.resultCount}`
+            countP.innerText = `Total count: ${entity.resultCount}`
             resultsDiv.appendChild(countP)
+
+
+            for (let track of entity.results) {
+                let trackP = document.createElement('p')
+                let trackLink = document.createElement('a')
+                trackLink.href = track.trackViewUrl
+                trackLink.innerText = track.trackName
+                trackLink.innerText = track.artistName
+                trackP.appendChild(trackLink)
+                resultsDiv.appendChild(trackP)
+
+            }
         }
-    })
+
+    });
 })
