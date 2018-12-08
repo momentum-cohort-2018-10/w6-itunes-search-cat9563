bee4ce03cbc096462af43aac1f698ece66b264ea
cat9563 <crysatl9563@gmail.com>
2018-12-08 14:57:46 -0500

    displays artist name and images

--- follow_along.js
+++ follow_along.js
@@ -1,28 +1,28 @@
-const searchButton = document.getElementById('search-button')
-const searchField = document.getElementById('search-field')
+// const searchButton = document.getElementById('search-button')
+// const searchField = document.getElementById('search-field')
 
-searchButton.addEventListener('click', function(event){//make ajax requet
-    $.ajax({
-        url: 'https://api.github.com/search/repositories',
-        data: {
-            q: searchField.value
-        },
-        success: function (results){
-            console.log(results)
-            let resultsDiv = document.getElementById('search-results')
-            resultsDiv.innerHTML = '' //clears stuff out 
-             let countP = document.createElement('p')
-            countP.innerText = `Total count: ${results.total_count}`
-            resultsDiv.appendChild(countP)
+// searchButton.addEventListener('click', function(event){//make ajax requet
+//     $.ajax({
+//         url: 'https://api.github.com/search/repositories',
+//         data: {
+//             q: searchField.value
+//         },
+//         success: function (results){
+//             console.log(results)
+//             let resultsDiv = document.getElementById('search-results')
+//             resultsDiv.innerHTML = '' //clears stuff out 
+//              let countP = document.createElement('p')
+//             countP.innerText = `Total count: ${results.total_count}`
+//             resultsDiv.appendChild(countP)
 
-            for (let repo of results.items){
-                let repoP = document.createElement("p") //creates a new element 
-                let repoLink =document.createElement('a') //found from console log 
-                repoLink.href = repo.html_url //had to look at log to get that also 
-                repoLink.innerText = repo.name //once again log 
-                repoP.appendChild(repoP) //puts the link inside repoP
-                resultsDiv.appendChild(repoP) //takes above line and appends it to the results div 
-            }
-        }
-    })
-})
+//             for (let repo of results.items){
+//                 let repoP = document.createElement("p") //creates a new element 
+//                 let repoLink =document.createElement('a') //found from console log 
+//                 repoLink.href = repo.html_url //had to look at log to get that also 
+//                 repoLink.innerText = repo.name //once again log 
+//                 repoP.appendChild(repoP) //puts the link inside repoP
+//                 resultsDiv.appendChild(repoP) //takes above line and appends it to the results div 
+//             }
+//         }
+//     })
+// })
--- itunes.js
+++ itunes.js
@@ -6,28 +6,35 @@ searchButton.addEventListener('click', function (event) {
     $.ajax({
         url: 'https://itunes-api-proxy.glitch.me/search?term=',
         data: {
-            term: searchField.value
+            term: searchField.value,
+            media: 'music'
         },
         dataType: "json",
         success: function (entity) {
             console.log(entity)
             let resultsDiv = document.getElementById('search-results')
+            resultsDiv.innerHTML = '' 
             let countP = document.createElement('p')
             countP.innerText = `Total count: ${entity.resultCount}`
             resultsDiv.appendChild(countP)
 
 
             for (let track of entity.results) {
-                let trackP = document.createElement('p')
-                let trackLink = document.createElement('a')
-                trackLink.href = track.trackViewUrl
-                trackLink.innerText = track.trackName
-                trackLink.innerText = track.artistName
-                trackP.appendChild(trackLink)
-                resultsDiv.appendChild(trackP)
-
+                let trackSongP = document.createElement("p")
+                let songLink = document.createElement("a")
+                let trackArtistP = document.createElement("p")
+                let coverPic = document.createElement("img")
+                coverPic.src = track.artworkUrl100
+                songLink.href = track.trackViewUrl
+                songLink.innerText = track.trackName
+                trackArtistP.innerText = track.artistName
+                trackSongP.appendChild(coverPic)
+                trackSongP.appendChild(songLink)
+                trackSongP.appendChild(trackArtistP)
+                resultsDiv.appendChild(trackSongP)
             }
         }
 
-    });
+    })
 })
+
