307b7531f1c41141a6aa6fb8573500b78d548664
cat9563 <crysatl9563@gmail.com>
2018-12-09 23:28:04 -0500

    figured how to manipulate dom information is in the right class's now can move forward and customize to fit into a grid

--- itunes.js
+++ itunes.js
@@ -1,18 +1,25 @@
 const searchButton = document.getElementById('search-button')
 const searchField = document.getElementById('search-field')
-
+const trackSongP = document.getElementById('trackSongP')
+const songLink = document.getElementById('songLink')
+const tackArtistP = document.getElementById('tackArtistP')
+const coverPic = document.getElementById('coverPic')
 
 searchButton.addEventListener('click', function (event) {
     $.ajax({
         url: 'https://itunes-api-proxy.glitch.me/search?term=',
         data: {
             term: searchField.value,
-            media: 'music'
+            media: 'music',
+            limit: 30
         },
         dataType: "json",
         success: function (entity) {
             console.log(entity)
             let resultsDiv = document.getElementById('search-results')
+            let songLinkDiv = document.getElementById('songLink')
+            let trackArtistPDiv = document.getElementById('trackArtistP')
+            let coverPicDiv = document.getElementById('coverPic')
             resultsDiv.innerHTML = '' 
             let countP = document.createElement('p')
             countP.innerText = `Total count: ${entity.resultCount}`
@@ -21,17 +28,22 @@ searchButton.addEventListener('click', function (event) {
 
             for (let track of entity.results) {
                 let trackSongP = document.createElement("p")
-                let songLink = document.createElement("a")
+                let artP = document.createElement("p")
+                let covP = document.createElement("p")
+                let songLinks = document.createElement("a")
                 let trackArtistP = document.createElement("p")
                 let coverPic = document.createElement("img")
                 coverPic.src = track.artworkUrl100
-                songLink.href = track.trackViewUrl
-                songLink.innerText = track.trackName
-                trackArtistP.innerText = track.artistName
-                trackSongP.appendChild(coverPic)
-                trackSongP.appendChild(songLink)
-                trackSongP.appendChild(trackArtistP)
-                resultsDiv.appendChild(trackSongP)
+                songLinks.href = track.trackViewUrl
+                songLinks.innerText = track.trackName
+                artP.innerText = track.artistName
+                covP.appendChild(coverPic)
+                trackSongP.appendChild(songLinks)
+                artP.appendChild(trackArtistP)
+                songLinkDiv.appendChild(trackSongP)
+                trackArtistPDiv.append(artP)
+                coverPicDiv.append(coverPic)
+                //resultsDiv.appendChild(trackSongP)
             }
         }
     })
